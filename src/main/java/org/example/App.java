1e5937e3ce1e1c7369ff30cf8e76a4dab48ad224
Rin Shimizu <52402142+rei-smz@users.noreply.github.com>
2024-02-28 18:35:11 -0500

    Passed "FizzBuzz",15

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,6 +12,10 @@ public class App
     }
 
     public static String fizzBuzz(int input) {
+        if (input % 3 == 0 && input % 5 == 0) {
+            return "FizzBuzz";
+        }
+
         if (input % 3 == 0) {
             return "Fizz";
         }
